009efd1d43ad8ca3efd68143bb2fbbc1a59ff329
Danmaster <danman19992@gmail.com>
2021-03-16 22:15:26 -0700

    Add to derp
    Add to derp

--- src/main/java/Method/Client/module/movement/Derp.java
+++ src/main/java/Method/Client/module/movement/Derp.java
@@ -24,8 +24,10 @@ public class Derp extends Module {
         super("Derp", Keyboard.KEY_NONE, Category.MOVEMENT, "Derp");
     }
 
-    Setting Silent=setmgr.add( new Setting("Silent", this, true));
-    Setting illegal=setmgr.add( new Setting("illegal Range?", this, false));
+    Setting Silent = setmgr.add(new Setting("Silent", this, true));
+    Setting Yaw = setmgr.add(new Setting("Yaw", this, true));
+    Setting Pitch = setmgr.add(new Setting("Pitch", this, true));
+    Setting illegal = setmgr.add(new Setting("illegal Range?", this, false));
 
     @Override
     public void onEnable() {
@@ -37,17 +39,17 @@ public class Derp extends Module {
     public void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
         int doubleit = illegal.getValBoolean() ? 2 : 1;
         if (!Silent.getValBoolean()) {
-            mc.player.rotationYaw = (float) ThreadLocalRandom.current().nextDouble(-180 * doubleit, 180 * doubleit);
-            mc.player.rotationPitch = (float) ThreadLocalRandom.current().nextDouble(-90 * doubleit, 90 * doubleit);
+            if (Yaw.getValBoolean())
+                mc.player.rotationYaw = (float) ThreadLocalRandom.current().nextDouble(-180 * doubleit, 180 * doubleit);
+            if (Pitch.getValBoolean())
+                mc.player.rotationPitch = (float) ThreadLocalRandom.current().nextDouble(-90 * doubleit, 90 * doubleit);
         }
 
     }
 
     @Override
     public void onPlayerMove(PlayerMoveEvent event) {
-        if (mc.gameSettings.keyBindSneak.isKeyDown()) {
-            mc.player.rotationYawHead = Utils.random(-180, 180);
-        }
+        mc.player.rotationYawHead = Utils.random(-180, 180);
     }
 
     @Override
@@ -56,8 +58,10 @@ public class Derp extends Module {
             int doubleit = illegal.getValBoolean() ? 2 : 1;
             if ((packet instanceof CPacketPlayer.Rotation || packet instanceof CPacketPlayer.PositionRotation) && side == Connection.Side.OUT) {
                 final CPacketPlayer packet2 = (CPacketPlayer) packet;
-                packet2.pitch = Utils.random(-180 * doubleit, 180 * doubleit);
-                packet2.yaw = Utils.random(-90 * doubleit, 90 * doubleit);
+                if (Pitch.getValBoolean())
+                    packet2.pitch = Utils.random(-180 * doubleit, 180 * doubleit);
+                if (Yaw.getValBoolean())
+                    packet2.yaw = Utils.random(-90 * doubleit, 90 * doubleit);
             }
         }
         return true;
